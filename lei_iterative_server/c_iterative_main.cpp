04381ccece24a9610a649e9c0fd9ac993a481362
shellmeng <mengqingxi89@126.com>
2013-05-13 23:36:56 +0800

    I need to pracitce how to build c plus plus class. So I want to pack my own class

---
+++ lei_iterative_server/c_iterative_main.cpp
@@ -0,0 +1,31 @@
+
+#include"c_iterative_server.h"
+int main()
+{
+
+     printf("start server\n");
+     int sockfd,connfd,n;
+     pid_t pid;
+     sockfd=buildConnection(PORT);
+     char  rec[MAX],send[MAX];
+     while(true)
+     {
+          connfd=accept(sockfd,NULL,NULL);
+              
+	  echo(connfd);
+         /* if((pid=fork())==0)
+          {
+               close(sockfd);
+               echo(connfd);
+          }
+          close(connfd);
+	  */
+     }
+     close(sockfd);
+
+}     
+
+
+
+
+
